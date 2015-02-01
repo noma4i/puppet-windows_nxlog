94440aebe854b56b247f36e70c77df998e939da5
Alexander Tsirel <noma4i@gmail.com>
2015-02-01 14:04:12 +1100

    puppet-lint --fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,15 @@
 define windows_nxlog (
   $ensure   = 'enabled',
-  $host = "127.0.0.1",
-  $port = "514"
+  $host = '127.0.0.1',
+  $port = '514'
 ){
   case $ensure {
     'enabled', 'present': {
       exec { 'cmd /c net stop nxlog || exit /b 0':
-        path => $::path,
+        path      => $::path,
         logoutput => false
       }->
-      download_file { "Download NXLog" :
+      download_file { 'Download NXLog' :
         url                   => 'http://nxlog.org/system/files/products/files/1/nxlog-ce-2.8.1248.msi',
         destination_directory => 'c:\temp',
         destination_file      => 'nxlog.msi'
@@ -23,10 +23,10 @@ define windows_nxlog (
       file { 'C:\\Program Files (x86)\\nxlog\\conf\\nxlog.conf':
         ensure             => file,
         source_permissions => ignore,
-        content             => template('windows_nxlog/nxlog.conf.erb')
+        content            => template('windows_nxlog/nxlog.conf.erb')
       }->
       exec { 'cmd /c net start nxlog  || exit /b 0':
-        path => $::path,
+        path      => $::path,
         logoutput => false
       }
     }
